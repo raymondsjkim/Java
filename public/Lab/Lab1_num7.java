96877a71980455868ca2ad55e02fd85ff26c2663
Raymond Kim <raymond.sj.kim@gmail.com>
2017-08-29 14:22:55 -0700

    Create Lab1_num7.java

---
+++ public/Lab/Lab1_num7.java
@@ -0,0 +1,29 @@
+/*  Program: Display Initial PP 1.10 pg. 54
+    Author: Raymond Kim
+    Class: Lab1_num7.java
+    Date: 8/29/2017
+    Description: Print out my initials using corresponding characters.
+    I certify that the code below is my own work.
+	Exception(s): N/A
+*/
+public class Lab1_num7 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("RRRRRRRRRRRRR	KKKK        KKKK\n"
+						 + "RRRRRRRRRRRRR   KKKK      KKKK\n"
+					     + "RRRR     RRRR   KKKK     KKKK\n"
+						 + "RRRR     RRRR   KKKK    KKKK\n"
+					     + "RRRR     RRRR   KKKK  KKKK\n"
+						 + "RRRRRRRRRRRRR   KKKK KKKK\n"
+					     + "RRRRRRRRRRRRR   KKKKKKK\n"
+						 + "RRRR RRRR       KKKKKKK\n"
+					     + "RRRR  RRRR      KKKK KKKK\n"
+						 + "RRRR   RRRR     KKKK  KKKK\n"
+					     + "RRRR    RRRR    KKKK   KKKK\n"
+						 + "RRRR     RRRR   KKKK    KKKK\n"
+					     + "RRRR      RRRR  KKKK     KKKK\n"
+						 + "RRRR       RRRR KKKK      KKKK\n"
+				); 
+	}
+}
