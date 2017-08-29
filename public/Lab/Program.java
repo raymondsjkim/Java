42d4f17de5a493f55436c1eb627d54bf01bb0f56
Raymond Kim <raymond.sj.kim@gmail.com>
2017-08-29 10:40:47 -0700

    Create Program.java

---
+++ public/Lab/Program.java
@@ -0,0 +1,23 @@
+/*  Program: Fix Errors
+    Author: Raymond Kim
+    Class: lab1
+    Date: 8/29/2017
+    Description: Debug errors and print out.
+
+    I certify that the code below is my own work.
+
+	Exception(s): N/A
+
+*/
+public class Problems {
+
+	public static void main (String[] args)
+	{
+	System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	System.out.println ("This program used to have lots of problems,");
+	System.out.println ("but if it prints this, you fixed them all.");
+	System.out.println (" *** Hurray! ***");
+	System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	} 
+
+}
