4c0cbe0d32dfc0466bbeaddeb9fd7f65463336e2
Raymond Kim <raymond.sj.kim@gmail.com>
2017-08-29 10:41:14 -0700

    Create Lab1.java

---
+++ public/Lab/Lab1.java
@@ -0,0 +1,22 @@
+/*  Program: Useful Facts
+    Author: Raymond Kim
+    Class: lab1
+    Date: 8/29/2017
+    Description: Prints out my name and a useful fact about me.
+
+    I certify that the code below is my own work.
+
+	Exception(s): N/A
+
+*/
+public class lab1 {
+		 // Prints some simple messages.
+		 public static void main (String[] args)
+		 {
+			 System.out.println("Author: Raymond Kim");
+			 System.out.println("Some useful facts:   I am an army veteran.");
+			 System.out.println("   I have some programming experience.");
+			 System.out.println("   I know HTML, CSS, JavaScript, jQuery, PHP, MySQL.");
+		 }
+		  
+}
