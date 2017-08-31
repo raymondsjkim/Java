68a9b4a4f5510c32c74fd67b7a71e5f206f98349
Raymond Kim <raymond.sj.kim@gmail.com>
2017-08-31 09:33:34 -0700

    Create NamesPlaces.java

---
+++ public/NamesPlaces.java
@@ -0,0 +1,19 @@
+public class Names {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println(" \tName\t\tHometown");
+		System.out.println("\t====\t\t========");
+		System.out.println("\tSally\t\tRoanoke");
+		System.out.println("\tAlexander\tWashington");
+		System.out.println();
+
+	}
+
+}
+
+//  	Name		Hometown
+	====		========
+	Sally		Roanoke
+	Alexander	Washington
