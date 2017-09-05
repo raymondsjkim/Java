2232f2e0636916507ac9f5ba0ed42c4fb9f4dafa
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-05 10:51:43 -0700

    Update Circle.java

--- public/Lab2/Circle.java
+++ public/Lab2/Circle.java
@@ -2,7 +2,7 @@
     Author: Raymond Kim
     Class: CSCI 145
     Date: 9/5/2017
-    Description: 
+    Description: This program calculates the circumference and area of a circle when the user enters a value for the radius.
 
     I certify that the code below is my own work.
 
@@ -10,23 +10,33 @@
 
 */
 
-public class Circle {
+import java.util.Scanner;
 
+public class Circle {
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in); 
+		
 		final double PI = 3.14159;
-		int radius = 10;
+		int radius;
+		
+		/* User enters the first value for the radius */
+		System.out.print("Please enter a value for the radius: ");
+		radius = scan.nextInt();
+	
 		double area1 = PI * radius * radius;
 		double circumference1 = 2 * PI * radius;
-
 		System.out.println("The area of a circle with radius " + radius +
-		" is " + area1);
+		" is " + area1);	
 		System.out.println("The circumference of a circle with radius " + radius +
 		" is " + circumference1);
 		
 		System.out.println();
 		
-		radius = 20;
+		/* User enters the second value for the radius */
+		System.out.print("Please enter another value for the radius: ");
+		radius = scan.nextInt();
+	
 		double area2 = PI * radius * radius;
 		double circumference2 = 2 * PI * radius;
 		System.out.println("The area of a circle with radius " + radius +
@@ -36,14 +46,11 @@ public class Circle {
 		
 		System.out.println();
 		
+		/* Calculates the factor by which the area and circumferences grew. */
 		double area_change = area2 / area1;
-		System.out.println("The factor by which the area grew is: " + area_change);
-		
 		double circum_change = circumference2 / circumference1;
+		System.out.println("The factor by which the area grew is: " + area_change);
 		System.out.println("The factor by which the circumference grew is: " 
-		+ circum_change);
-		
-		
+		+ circum_change);		
 	}
-
 }
