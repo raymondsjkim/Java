ec3d939c0b63743946c6e8d37627f0788a3fecca
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-05 09:36:45 -0700

    Create Lab2.java

---
+++ public/Lab/Lab2.java
@@ -0,0 +1,18 @@
+/**
+  THIS IS PRACTICE
+*/
+
+public class Lab2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println ("This is a long string that is the " +
+				"concatenation of two shorter strings.");
+		System.out.println ("The first computer was invented about" + 55 +
+				"years ago.");
+		System.out.println ("8 plus 5 is " + 8 + 5); // 8 plus 5 is 85
+		System.out.println ("8 plus 5 is " + (8 + 5)) ; // 8 plus 5 is 13
+		System.out.println (8 + 5 + " equals 8 plus 5."); // 13 equals 8 plus 5
+	}
+
+}
