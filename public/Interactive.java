d513a8f781ffcb453ef3f302f9b2be07ef174de6
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-05 10:30:44 -0700

    Update Interactive.java

--- public/Interactive.java
+++ public/Interactive.java
@@ -6,18 +6,27 @@ public class Interactive {
 
 	      int miles;
 	      double gallons, mpg;
+	      String model;
 
 	      Scanner scan = new Scanner(System.in);
+	      
+	      System.out.print("Enter the model: ");
+	      // takes in a String	
+	      model = scan.next();
+	      
 	      System.out.print("Enter the number of miles: ");
+	      // takes in a int		
 	      miles = scan.nextInt();
 
 	      System.out.print("Enter the gallons of fuel used: ");
+	      // takes in a double		
 	      gallons = scan.nextDouble();
 
 	      mpg = miles / gallons;
-        
+
+	      System.out.println("Model: " + model);
 	      System.out.println("Miles Per Gallon: " + mpg);
-        
+
 	      scan.close();
 	}
 
