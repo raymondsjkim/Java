8b9d4aedd40059f4735bbc2e014f94c02588ead2
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-07 10:51:52 -0700

    Rename Errors.java to Errors2.java

---
+++ Errors2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Errors {
+
+	public static void main(String[] args) {
+		
+		String Name; // Name of the user
+		int number;
+		int numSq;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter your name, please: ");
+		Name = scan.next();
+		System.out.print("What is your favorite number? ");
+		number = scan.nextInt();
+		System.out.println();
+		
+		numSq = number * number;
+		
+		System.out.println (Name + ", the square of your number is "
+		 + numSq);
+	}
+
+}
