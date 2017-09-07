e6fc953461af44e45db3ad1e20e1d192698617ef
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-06 22:56:54 -0700

    Update LabGrade.java

--- public/Lab2/LabGrade.java
+++ public/Lab2/LabGrade.java
@@ -1,3 +1,11 @@
+/*  
+  	Program: LabGrade.java
+    Author: Raymond Kim
+    Class: CSCI 145
+    Date: 9/6/2017
+    Description: This program calculates your lab grade.
+	Exception(s): N/A
+*/
 import java.util.Scanner;
 
 public class LabGrade
@@ -5,8 +13,8 @@ public class LabGrade
 	public static void main (String[] args)
 	{
 		// Declare constants
-		double in_weight = 0.6; // in-class weight is 60%
-		double out_weight = 0.4; // out-of-class weight is 40%
+		double inWeight; // in-class weight is 60%
+		double outWeight; // out-of-class weight is 40%
 		// Declare variables
 		int preLabPts; //number of points earned on the pre-lab assignment
 		int preLabMax; //maximum number of points possible for pre-lab
@@ -20,26 +28,44 @@ public class LabGrade
 		Scanner scan = new Scanner(System.in);
 		// Get the input
 		System.out.println("\nWelcome to the Lab Grade Calculator\n");
+		
+		
+		System.out.print("Enter in the in-class weight in decimal form:");
+		inWeight = scan.nextDouble();
+		
+		System.out.print("Enter in the out-class weight in decimal form: ");
+		outWeight = scan.nextDouble();
+		
 		System.out.print("Enter the number of points you earned on the pre-lab: ");
 		preLabPts = scan.nextInt(); // 17
+		
 		System.out.print("What was the maximum number of points you could have earned? ");
 		preLabMax = scan.nextInt(); // 20
+		
 		System.out.print("Enter the number of points you earned on the lab: ");
 		labPts = scan.nextInt(); // 23
+		
 		System.out.print("What was the maximum number of points for the lab? ");
 		labMax = scan.nextInt(); // 25
+		
 		System.out.print("Enter the number of points you earned on the post-lab: ");
 		postLabPts = scan.nextInt(); // 12
+		
 		System.out.print("What was the maximum number of points for the post-lab? ");
 		postLabMax = scan.nextInt(); // 15
+		
 		System.out.println();
+		
 		// Calculate the average for the out of class work
 		outClassAvg = 100 * (preLabPts + postLabPts) / (preLabMax + postLabMax);
+		
 		// Calculate the average for the in-class work
 		inClassAvg = 100 * labPts / labMax;
+		
 		// Calculate the weighted average taking 40% of the out-of-class average
 		// plus 60% of the in-class
-		labGrade = out_weight * outClassAvg + in_weight * inClassAvg;
+		labGrade = outWeight * outClassAvg + inWeight * inClassAvg;
+		
 		// Print the results
 		System.out.println("Your average on out-of-class work is " + outClassAvg + "%");
 		System.out.println("Your average on in-class work is " + inClassAvg + "%");
