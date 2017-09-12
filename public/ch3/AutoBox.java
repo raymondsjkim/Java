7b64eb54a208fdf2189a1704958868cdeb17468b
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-12 08:34:34 -0700

    Create AutoBox.java

---
+++ public/ch3/AutoBox.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ *	Autoboxing is the automatic conversion of a primitive value to a corresponding wrapper object
+ * 
+ */
+public class AutoBox {
+	public static void main(String[] args) {
+		
+		Integer obj;
+		int num = 43; // primitive value
+		obj = num; // integer object
+		
+		System.out.println(obj);
+
+	}
+}
