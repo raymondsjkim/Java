e68ae61367ed87b86eb95c178e00300b2d96d7b8
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-13 23:40:39 -0700

    Create Initials.java

---
+++ public/Lab3/Initials.java
@@ -0,0 +1,10 @@
+package initials;
+public class initials {
+	public static void main(String[] args) {
+		System.out.println("####### #    # #######");
+		System.out.println("#     # #  #   #");
+		System.out.println("####### ##     #######");
+		System.out.println("#   #   #  #   #     #");
+		System.out.println("#    #  #    # #######");
+	}
+}
