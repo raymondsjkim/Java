66d871f63b412112b0fbdd4ff86443f185a91080
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-14 09:43:06 -0700

    Update IntWrapper.java

--- public/Lab3/IntWrapper.java
+++ public/Lab3/IntWrapper.java
@@ -1,8 +1,18 @@
+/*  
+  	Program: Experimenting with the Integer Class
+    Author: Raymond Kim
+    Class: CSCI 145
+    Date: 9/12/2017
+    Description: This program returns the Binary, Octal, and Hexadecimal numbers from an integer using wrapper classes. 
+    It returns the max and min Java integer values. It also takes in to string values, converts them to integers using 
+    parseInt(), and sums the two values.
+	Exception(s): N/A
+*/
 import java.util.Scanner;
 public class IntWrapper {
-
 	public static void main(String[] args) {
-		int read_int, num1, num2;
+		int read_int, num1Int, num2Int;
+		String num1, num2;
 		Scanner scan = new Scanner(System.in);
 		// Prompt for and read in an integer to convert
 		System.out.print("Enter in a integer to convert to Binary, Octal, and Hexadecimal: ");
@@ -10,17 +20,18 @@ public class IntWrapper {
 		System.out.println();
 		// Prompt the user to enter two decimal integers, one per line for calculation
 		System.out.print("Enter in number for x: ");
-		num1 = scan.nextInt();
+		num1 = scan.next();
 		System.out.print("Enter in number for y: ");
-		num2 = scan.nextInt();
-		// Declare and instantiate the integer objects
+		num2 = scan.next();
+		// Declare and instantiate the integer object
 		Integer num = new Integer(read_int);
-		Integer num1Obj = new Integer(num1);
-		Integer num2Obj = new Integer(num2);
 		// Convert to binary, octal, and hexadecimal
         String binary = Integer.toBinaryString(num);
         String octal = Integer.toOctalString(num);
         String hexadecimal = Integer.toHexString(num);
+        // Parse strings to integers
+        num1Int = Integer.parseInt(num1);
+        num2Int = Integer.parseInt(num2);
         System.out.println();
         // print the binary, octal and hexadecimal 
         System.out.println("Binary value: " + binary);
@@ -28,13 +39,11 @@ public class IntWrapper {
         System.out.println("Hexadecimal value: " + hexadecimal);
         System.out.println();
         // Print the maximum and minimum possible Java integer values
-        System.out.println("Max possible Java integer value: " + Integer.MAX_VALUE); 
-        System.out.println("Min possible Java integer value: " + Integer.MIN_VALUE); 
+        System.out.println("Max. possible Java integer value: " + Integer.MAX_VALUE); 
+        System.out.println("Min. possible Java integer value: " + Integer.MIN_VALUE); 
         System.out.println();
-//        System.out.println(num2Obj.getClass().getName());
-        System.out.println("The total of x and y is: " + (num1Obj + num2Obj));
-
-		System.exit(0);
+        // Print the total of x and y when they are strings and when they are parsed into integers
+        System.out.println("This is adds the strings x and y: " + (num1 + num2));
+        System.out.println("This is adds the integers x and y: " + (num1Int + num2Int));
 	}
-
 }
