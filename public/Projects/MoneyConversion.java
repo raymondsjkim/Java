089df76a1c606211b46b8835275829bb3974d6f4
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-16 12:32:53 -0700

    Update MoneyConversion.java

--- public/Projects/MoneyConversion.java
+++ public/Projects/MoneyConversion.java
@@ -2,49 +2,35 @@ import java.util.Scanner;
 public class MoneyConversion {
 	public static void main(String[] args) {
         double amount;
-        int one, five, ten;
-        int penny, nickel, dime, quarter;
+        int one, five, ten, twenty, penny, nickel, dime, quarter;
         Scanner scan= new Scanner(System.in);
-       
         System.out.print("What is the amount? ");
-        amount = scan.nextDouble();
-        
+        amount = scan.nextDouble(); // User input 57.63
+        twenty = (int) amount / 20;
+        amount = amount % 20; // amount 17.63
         ten = (int) amount / 10;
-        amount = amount % 10;
-        System.out.println ("Left over amount when 10 is taken out " + amount);
-       
+        amount = amount % 10; // amount is now 7.63
         five = (int) amount / 5;
-        amount = amount % 5;
-        System.out.println ("Left over amount when 5 is taken out " + amount);
-       
+        amount = amount % 5; // amount is now 2.63
         one = (int) amount / 1;
-        amount = amount % 1;
-        System.out.println ("Left over amount when 1 is taken out " + amount);
-       
+        amount = amount % 1;// amount is now 0.63
         quarter = (int) (amount / 0.25);
-        amount = amount % 25;
-        System.out.println ("Left over amount when .25 is taken out " + amount);
-        
+        amount = amount % 0.25; // amount is now 0.13
         dime = (int) (amount / 0.10);
-        amount = amount % 10;
-        System.out.println ("Left over amount when .10 is taken out " + amount);
-       
+        amount = amount % 0.10; // amount is now 0.03
         nickel = (int) (amount / 0.5);
-        amount = amount % 5;
-        System.out.println ("Left over amount when .5 is taken out " + amount);
-       
+        amount = amount % 0.5; // amount is still 0.03
         penny = (int) (amount / 0.01);
-        amount = amount % 1;
-        System.out.println ("Left over amount when .01 is taken out " + amount);
-        
+        amount = amount % 0.01; // amount is still 0.01
+        // Print output
         System.out.println ();
-        
-        System.out.println (ten + " ten dollar bills");
-        System.out.println (five + " five dollar bills");
-        System.out.println (one + " one dollar bills");
-        System.out.println (quarter + " quarters");
-        System.out.println (dime +  " dimes");
-        System.out.println (nickel + " nickels");
-        System.out.println (penny + " pennies");
+        System.out.println (twenty + " twenty-dollar bill(s)");
+        System.out.println (ten + " ten-dollar bill(s)");
+        System.out.println (five + " five-dollar bill(s)");
+        System.out.println (one + " one-dollar bill(s)");
+        System.out.println (quarter + " quarter(s)");
+        System.out.println (dime +  " dime(s)");
+        System.out.println (nickel + " nickel(s)");
+        System.out.println (penny + " pennie(s)");
 	}
 }
