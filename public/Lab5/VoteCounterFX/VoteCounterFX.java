04cb6969436e1d7c2b9bd7ad9e76eacfb3e999da
Raymond Kim <raymond.sj.kim@gmail.com>
2017-09-26 10:19:09 -0700

    Create VoteCounterFX.java

---
+++ public/Lab5/VoteCounterFX/VoteCounterFX.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Created by T. Vo using a previous edition of Lewis book
+// Modified by:
+
+// Demonstrates a graphical user interface and event
+// listeners to tally votes for two candidates, Joe and Sam.
+
+public class VoteCounterFX extends Application
+{
+    public void start(Stage primaryStage)
+    {
+        Scene scene = new Scene(new VoteCounterPaneFX(), 300, 150);
+        
+        primaryStage.setTitle("Vote Counter");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
