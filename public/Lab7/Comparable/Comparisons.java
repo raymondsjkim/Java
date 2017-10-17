060dedf8083fe3e4434fdd074a435616f826404c
Raymond Kim <raymond.sj.kim@gmail.com>
2017-10-17 10:04:08 -0700

    Update Comparisons.java

--- public/Lab7/Comparable/Comparisons.java
+++ public/Lab7/Comparable/Comparisons.java
@@ -1,26 +1,35 @@
+
 import java.util.Scanner;
-
 public class Comparisons {
-
 	public static void main(String[] args) {
-		
-		String one, two, three;
-		Scanner scan = new Scanner(System.in);
-		
-		
-		System.out.print("Enter one: ");
-		one = scan.nextLine();
-		System.out.print("Enter two: ");
-		two = scan.nextLine();
-		System.out.print("Enter three: ");
-		three = scan.nextLine();
-		
-		Compare3 comp = new Compare3(one, two, three);
-		
-		
-		System.out.print(comp);
-		
-		
-	}
+	       	String val1;
+	        String val2;
+	        String val3;
+	        int s1, s2, s3;
+	        Scanner scan= new Scanner(System.in);
+	        // Prompt user
+	        System.out.print("Please enter the first string:");
+	        val1 = scan.nextLine();
+	        System.out.print("Please enter the second string:");
+	        val2 = scan.nextLine();
+	        System.out.print("Please enter the third string:");
+	        val3 = scan.nextLine();
+	        // Call and print the largest
 
+	        
+	        // Prompt user
+	        System.out.print("Please enter the first integer:");
+	        s1 = scan.nextInt();
+	        System.out.print("Please enter the second integer:");
+	        s2 = scan.nextInt();
+	        System.out.print("Please enter the third integer:");
+	        s3 = scan.nextInt();
+	        
+	        System.out.println();
+	        
+	        System.out.println("The largest string is: " + Compare3.largest(val1,val2,val3));
+	        System.out.println("The largest integer is: " + Compare3.largest(s1,s2,s3));
+	        
+
+	}
 }
