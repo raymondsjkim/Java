77a8c1629431d89b371e68008271c410eea1fb8e
Raymond Kim <raymond.sj.kim@gmail.com>
2017-10-16 21:42:26 -0700

    Update Roulette.java

--- public/Project2/Roulette.java
+++ public/Project2/Roulette.java
@@ -9,31 +9,47 @@ import java.util.*;
 //************************************************************************
 class Roulette
 {
-    //=====================================================================
+    private static final Wheel Wheel = null;
+
+	//=====================================================================
     //  Contains the main processing loop for the roulette game.
     //=====================================================================
     public static void main (String[] args)
     {
       	Scanner scan = new Scanner(System.in);
       	Player player1 = new Player ("Jane", 100);   // $100 to start for Jane
+      	Player player2 = new Player ("Ray", 100);
       	boolean done = false;
-
+      	boolean b1 = true;
+      	boolean b2 = true;
+      	
       	System.out.println ("Author: your Raymond Kim");
       	Wheel.welcomeMessage();
 
       	while (!done)
       	{
+//      		if(b1) {
+//      			
+//      		}
          	System.out.println ("Money available for " + player1.getName()
                  	+ ": " + player1.getMoney());
+         
+         	System.out.println();
          	
-         	player1.makeBet(scan);
+         	player1.makeBet(scan); // call makeBet method
+         	
          	System.out.println();
          	Wheel.spin(); // call spin
+         	
+         	
+         	player1.payment();
          	System.out.println();
          	done = !player1.playAgain(scan);
          	System.out.println();
       	}
-
+      	
+      	System.out.println(player1.displayStatus());
+      	System.out.println();
       	System.out.println ("Game over!  Thanks for playing.");
       	scan.close();
    }
