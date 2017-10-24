4f51b265bbd59dae028011243236332b81bea3ff
Raymond Kim <raymond.sj.kim@gmail.com>
2017-10-24 08:40:18 -0700

    Update Shop.java

--- public/lab8/Shop/Shop.java
+++ public/lab8/Shop/Shop.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class Shop {
 
 	public static void main(String[] args) {
 	      ShoppingCart cart = new ShoppingCart();
+	      NumberFormat fmt = NumberFormat.getCurrencyInstance();
 	      String itemName;
 	      double itemPrice;
 	      double totalPrice = 0;
 	      int quantity;
 	      Scanner scan = new Scanner(System.in);
-	      String keepShopping = "y";
+	      String shopMore = "y";
 	    
 	      do
 	         {
+	    	   System.out.println();
 	           System.out.print("Enter the name of the item: ");
 	           itemName = scan.next();
-	           
 	           System.out.print("Enter the unit price: ");
 	           itemPrice = scan.nextDouble();
-	           
 	           System.out.print("Enter the quantity: ");
 	           quantity = scan.nextInt();
-	           
-	           totalPrice +=(quantity*itemPrice);
+	           // calculate total price
+	           totalPrice = totalPrice + (quantity*itemPrice);
+	           // pass in user input in to the method
 	           cart.addToCart(itemName, itemPrice, quantity);
-	           
-	           System.out.println(cart.toString());
-	           
-	           System.out.println("Continue shoppping (y/n)?");
-	           keepShopping = scan.next();
-	          }
-	      while (keepShopping.equals("y"));
-	      System.out.println("Please pay $"+ totalPrice);
+	           // Print the cart
+	           System.out.println(cart);
+	           // print the total price
+	           System.out.println("Your total is: " + fmt.format(totalPrice));
+	           System.out.println();
+	           System.out.print("Continue shoppping (y/n)?");
+	           shopMore = scan.next();
+	          } while (shopMore.equals("y"));
+	      
+	      System.out.println();
+	      System.out.println("Please pay "+ fmt.format(totalPrice));
 	}
 }
