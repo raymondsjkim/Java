0a0285148e4adaba48b70cfd62e8ea88c9b52a59
Raymond Kim <raymond.sj.kim@gmail.com>
2017-11-09 09:46:43 -0800

    Update Square.java

--- public/lab10/Square.java
+++ public/lab10/Square.java
@@ -1,28 +1,106 @@
 import java.util.Scanner;
-import java.io.IOException;
-import java.io.File;
-
-public class SquareTest
+ 
+public class Square
 {
-	public static void main(String[] args) throws IOException
-	{
-		Scanner scan = new Scanner(new File("magicData"));
-		int count = 1; //count which square we're on
-		int size = scan.nextInt(); //size of next square
-		//Expecting -1 at bottom of input file
-		while (size != -1)
-		 {
-			//create a new Square of the given size
-			//call its read method to read the values of the square
-			System.out.println("\n******** Square " + count + " ********");
-			//print the square
-			//print the sums of its rows
-			//print the sums of its columns
-			//print the sum of the main diagonal
-			//print the sum of the other diagonal
-			//determine and print whether it is a magic square
-			//get size of next square
-			size = scan.nextInt();
-		 }
-	}
+    int[][] square;
+ 
+    //--------------------------------------
+    //create new square of given size
+    //--------------------------------------
+    public Square(int size)
+    {
+      square = new int[size][size];
+    }
+ 
+    //--------------------------------------
+    //return the sum of the values in the given row
+    //--------------------------------------
+    public int sumRow(int row)
+    {
+      int sum = 0;
+      for(int i = 0; i < square.length; i++)
+            sum += square[row][i];
+            return sum;
+    }
+ 
+    //--------------------------------------
+    //return the sum of the values in the given column
+    //--------------------------------------
+    public int sumCol(int col)
+    {
+      int sum = 0;
+      for(int i = 0; i < square.length; i++)
+            sum += square[i][col];
+      return sum;
+    }
+ 
+    //--------------------------------------
+    //return the sum of the values in the main diagonal
+    //--------------------------------------
+    public int sumMainDiag()
+    {
+      int sum = 0;
+      for(int i = 0; i < square[0].length; i++)
+            sum += square[i][i];
+      return sum;
+    }
+ 
+    //--------------------------------------
+    //return the sum of the values in the other ("reverse") diagonal
+    //--------------------------------------
+    public int sumOtherDiag()
+    {
+      int sum = 0;
+      int x = square.length;
+      for(int i = 0; i < x; i++)
+            sum += square[x-1-i][i];
+      return sum;
+    }
+ 
+    //--------------------------------------
+    //return true if the square is magic (all rows, cols, and diags have
+    //same sum), false otherwise
+    //--------------------------------------
+    public boolean magic(int[] sums)
+    {
+      boolean bool = true;
+      for(int i = 0; i < sums.length; i++)
+      {
+            if(i + 1 != sums.length)
+            {
+                  if(sums[i] == sums[i+1])
+                        bool = true;
+                  else
+                  {
+                        i = sums.length;
+                        bool = false;
+                  }
+            }    
+      }
+      return bool;
+    }
+ 
+    //--------------------------------------
+    //read info into the square from the input stream associated with the
+    //Scanner parameter
+    //--------------------------------------
+    public void readSquare(Scanner scan)
+    {
+      for (int row = 0; row < square.length; row++)
+        for (int col = 0; col < square.length; col ++)
+          square[row][col] = scan.nextInt();
+    }
+ 
+    //--------------------------------------
+    //print the contents of the square, neatly formatted
+    //--------------------------------------
+    public void printSquare() {
+      for(int row = 0; row < square.length; row++) {
+	      for (int col = 0; col < square.length; col++) {
+	            System.out.print(square[row][col] + " ");
+	      }
+	     System.out.println();
+      }
+    }
+ 
 }
