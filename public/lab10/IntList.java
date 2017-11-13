100d2bc96b813b1ae4a56ff1ecd1d3ff84291c6b
Raymond Kim <raymond.sj.kim@gmail.com>
2017-11-13 14:04:31 -0800

    Create IntList.java

---
+++ public/lab10/IntList.java
@@ -0,0 +1,39 @@
+public class IntList
+{
+	protected int[] list;
+	protected int numElements = 0;
+	//-------------------------------------------------------------
+	// Constructor -- creates an integer list of a given size.
+	//-------------------------------------------------------------
+	public IntList(int size)
+	{
+		list = new int[size];
+	}
+	//------------------------------------------------------------
+	// Adds an integer to the list. If the list is full,
+	// prints a message and does nothing.
+	//------------------------------------------------------------
+	public void add(int value)
+	{
+		if (numElements == list.length)
+			System.out.println("Can't add, list is full");
+		else
+		 {
+			list[numElements] = value;
+			numElements++;
+		 }
+
+	 }
+	 //-------------------------------------------------------------
+	 // Returns a string containing the elements of the list with their
+	 // indices.
+	 //-------------------------------------------------------------
+	 public String toString()
+	 {
+		 String returnString = "";
+		 for (int i = 0; i < numElements; i++) {
+			 returnString = returnString + i + ": " + list[i] + "\n";
+		 }
+		 return returnString;
+	 }
+ } 
